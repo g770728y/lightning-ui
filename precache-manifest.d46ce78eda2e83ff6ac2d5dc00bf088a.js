96004fd1f78688e55ba4f4847f2f492f3f694352
g770728y <g770728y@163.com>
2019-02-23 15:25:40 +0800

    Updates

---
+++ precache-manifest.d46ce78eda2e83ff6ac2d5dc00bf088a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lightning-ui/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a18220dd4460f09b6a73",
+    "url": "/lightning-ui/static/js/runtime~main.a18220dd.js"
+  },
+  {
+    "revision": "e44c09db3c07848fa101",
+    "url": "/lightning-ui/static/js/main.e44c09db.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/lightning-ui/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "e44c09db3c07848fa101",
+    "url": "/lightning-ui/static/css/main.7239a48d.chunk.css"
+  },
+  {
+    "revision": "5f652a57ea3f63d32dac8fe8a6ef3001",
+    "url": "/lightning-ui/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c0698adedcefed558103dce6ce0cbd0.js"
+  "/lightning-ui/precache-manifest.d46ce78eda2e83ff6ac2d5dc00bf088a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/lightning-ui/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
